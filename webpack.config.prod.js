9937a314e33587bee1bb5bd452e9095eb1e02593
Justin <jvcruz@ucdavis.edu>
2017-12-03 23:48:41 -0800

    attempted production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,7 +22,7 @@ const config = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'production',
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
   ],
